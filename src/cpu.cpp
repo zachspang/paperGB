e9d3a321356885a3ec0510eff731ddcd99bcbb7a
zachspang <spangzach@gmail.com>
2025-10-22 16:31:19 -0700

    Add headers for all cpu instructions

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,16 +1,41 @@
 #include "cpu.h"
 
+uint16_t ByteRegisterPair::get_word() {
+	return (high << 8) | low;
+}
+
+void ByteRegisterPair::set_word(uint16_t set) {
+	low = set & 0xFF;
+	high = (set >> 8) & 0xFF;
+}
+
+uint16_t WordRegister::get_word() {
+	return word;
+}
+
+void WordRegister::set_word(uint16_t set) {
+	word = set;
+}
+
 CPU::CPU(GB& in_gb) :
 	gb(in_gb)
 {
-	A = 0;
-	F = 0;
-	B = 0;
-	C = 0;
-	D = 0;
-	E = 0;
-	H = 0;
-	L = 0;
-	SP = 0;
-	PC = 0;
+	AF.high = 0;
+	AF.low = 0;
+	BC.high = 0;
+	BC.low = 0;
+	DE.high = 0;
+	DE.low = 0;
+	HL.high = 0;
+	HL.low = 0;
+	SP.word = 0;
+	PC.word = 0;
+}
+
+void CPU::LD(uint8_t& dest, uint8_t operand) {
+	dest = operand;
+}
+
+void CPU::LD(Register& dest, uint16_t operand) {
+	dest.set_word(operand);
 }
--- src/cpu.h
+++ src/cpu.h
@@ -1,5 +1,25 @@
 #include <cstdint>
 #include "gb.h"
+class Register {
+public:
+	virtual uint16_t get_word() = 0;
+	virtual void set_word(uint16_t set) = 0;
+};
+
+class ByteRegisterPair : public Register {
+public:
+	uint8_t high;
+	uint8_t low;
+	uint16_t get_word();
+	void set_word(uint16_t set);
+};
+
+class WordRegister : public Register {
+public:
+	uint16_t word;
+	uint16_t get_word();
+	void set_word(uint16_t set);
+};
 
 class CPU {
 public:
@@ -9,36 +29,193 @@ public:
 
 	void execute_CB_opcode(uint8_t opcode);
 
+	//CPU Instructions
+	//Info on instructions with not obvious timings 
+	// https://gist.github.com/SonoSooS/c0055300670d678b5ae8433e20bea595#fetch-and-stuff
+
+	//Add operand to A + carry flag
+	void ADC(uint8_t operand);
+
+	//Add operand to A
+	void ADD(uint8_t operand);
+
+	//Add operand to dest
+	void ADD(Register& dest, uint16_t operand);
+
+	//Bitwise and dest with operand
+	void AND(uint8_t& dest, uint8_t operand);
+
+	//Test the bit_idx(0-7) bit of operand and set the zero flag if the bit is not set
+	void BIT(uint8_t bit_idx, uint8_t operand);
+
+	//Call addr
+	void CALL(uint16_t addr);
+
+	//Call addr if cond is met
+	void CALL(uint8_t cond, uint16_t addr);
+
+	//Complement carry flag
+	void CCF();
+
+	//Compare the value in A with operand, subtract operand from A and set flags then discard result
+	void CP(uint8_t operand);
+
+	//Complement A
+	void CPL();
+
+	//Decimal Adjust Accumulator
+	//https://rgbds.gbdev.io/docs/v0.9.4/gbz80.7#DAA
+	void DAA();
+
+	//Decrement dest
+	void DEC(uint8_t& dest);
+
+	//Decrement dest
+	void DEC(Register& dest);
+
+	//Disable interrupts byte clearing IME flag
+	void DI();
+	
+	//Enable interrupts by setting IME flag
+	void EI();
+
+	//Enter CPU low-power mode utill an interrupt occurs
+	//https://rgbds.gbdev.io/docs/v0.9.4/gbz80.7#HALT
+	void HALT();
+
+	//Increment dest
+	void INC(uint8_t& dest);
+
+	//Increment Dest
+	void INC(Register& dest);
+
+	//Jump to addr
+	void JP(uint16_t addr);
+
+	//Jump to addr if cond is met
+	void JP(uint8_t cond, uint16_t addr);
+
+	//Relative jump to PC + offset(-128,+127)
+	void JR(int8_t offset);
+
+	//Relative jump to PC + offset(-128,+127) if cond is met
+	void JR(uint8_t cond, int8_t offset);
+
+	//Copy value of operand to dest
+	void LD(uint8_t& dest, uint8_t operand);
+
+	//Copy value of operand to dest
+	void LD(Register& dest, uint16_t operand);
+
+	//Copy value of operand to dest then increment HL
+	void LD_HLI(uint8_t& dest, uint8_t operand);
+
+	//Copy value of operand to dest then decrement HL
+	void LD_HLD(uint8_t& dest, uint8_t operand);
+
+	//No operation
+	void NOP();
+
+	//Bitwise or A and operand
+	void OR(uint8_t operand);
+
+	//Pop from stack and store to dest
+	void POP(Register& dest);
+
+	//Push to stack from dest
+	void PUSH(Register& dest);
+
+	//Set bit bit_idx(0-7) to 0
+	void RES(uint8_t bit_idx, uint8_t& dest);
+
+	//Return from subroutine
+	void RET();
+
+	//Return from subroutine if cond is met
+	void RET(uint8_t cond);
+
+	//Return from subroutine and enable interrupts
+	void RETI();
+
+	//Rotate bits of operand left through the carry flag
+	void RL(uint8_t& operand);
+
+	//Rotate bits of A left through the carry flag
+	void RLA();
+
+	//Rotate bits of operand left
+	void RLC(uint8_t& operand);
+
+	//Rotate bits of A left
+	void RLCA();
+
+	//Rotate bits of operand right through the carry flag
+	void RR(uint8_t& operand);
+
+	//Rotate bits of A right through the carry flag
+	void RRA();
+
+	//Rotate bits of operand right
+	void RRC(uint8_t& operand);
+
+	//Rotate bits of A right
+	void RRCA();
+
+	//Call address tgt from RST vector
+	void RST(uint8_t tgt);
+	
+	//Subtract operand and carry flag from A
+	void SBC(uint8_t operand);
+
+	//Set carry flag
+	void SCF();
+
+	//Set bit bit_idx(0-7) of dest
+	void SET(uint8_t bit_idx, uint8_t& dest);
+
+	//Shift dest left arithmetically
+	void SLA(uint8_t& dest);
+
+	//Shift dest right arithmetically
+	void SRA(uint8_t& dest);
+
+	//Shift dest right logically
+	void SRL(uint8_t& dest);
+
+	//STOP is weird and not used in any licensed games, for now it will just call NOP
+	void STOP();
+
+	//Subtract operand from A
+	void SUB(uint8_t operand);
+
+	//Swap the upper and lower bits of dest
+	void SWAP(uint8_t& dest);
+
+	//Bitwise XOR A and operand
+	void XOR(uint8_t operand);
+
 private:
 	//GB object, used to access memory
 	GB& gb;
 
 	//Registers AF, BC, DE, HL are 16bit registers that can also be two seperate 8bit registers
 
-	//Accumulator register
-	uint8_t A;
-	//Flags register, bits 0-3 are empty, 4:carry flag, 5:half carry flag, 6: subtraction flag, 7: zero flag
-	uint8_t F;
+	//Accumulator register and Flags register, bits 0-3 are empty, 4:carry flag, 5:half carry flag, 6: subtraction flag, 7: zero flag
+	ByteRegisterPair AF;
 
-	//Register B
-	uint8_t B;
-	//Register C
-	uint8_t C;
+	//Register B and C
+	ByteRegisterPair BC;
 
-	//Register D
-	uint8_t D;
-	//Register E
-	uint8_t E;
+	//Register D and E
+	ByteRegisterPair DE;
 
-	//Register H
-	uint8_t H;
-	//Register L
-	uint8_t L;
+	//Register H and L
+	ByteRegisterPair HL;
 
 	//Stack pointer
-	uint16_t SP;
+	WordRegister SP;
 
 	//Program counter
-	uint16_t PC;
+	WordRegister PC;
 	
 };
