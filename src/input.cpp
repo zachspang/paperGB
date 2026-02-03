c68e00ac3b93fad23adf17d710d56dfa7e48f11f
zachspang <spangzach@gmail.com>
2026-02-03 14:04:53 -0800

    Implement joypad input

--- src/input.cpp
+++ src/input.cpp
@@ -5,9 +5,37 @@ Input::Input() {
 }
 
 uint8_t Input::read_joypad() {
+	update_buttons();
 	return joypad_input;
 };
 
 void Input::write_joypad(uint8_t byte) {
-	joypad_input = (byte & 0b00110000) | (joypad_input & 0b11001111);
+	joypad_input = (byte & 0b11110000);
 };
+
+void Input::update_buttons() {
+	joypad_input &= 0b11110000;
+
+	//If SsBA selected
+	if (((joypad_input >> 5) & 1) == 0) {
+		start = !keyboard_state[SDL_SCANCODE_GRAVE];
+		select = !keyboard_state[SDL_SCANCODE_TAB];
+		a = !keyboard_state[SDL_SCANCODE_SPACE];
+		b = !keyboard_state[SDL_SCANCODE_R];
+
+		joypad_input |= start << 3 | select << 2 | b << 1 | a;
+	}
+	//If d-pad selected
+	else if (((joypad_input >> 4) & 1) == 0) {
+		up = !keyboard_state[SDL_SCANCODE_W];
+		down = !keyboard_state[SDL_SCANCODE_S];
+		left = !keyboard_state[SDL_SCANCODE_A];
+		right = !keyboard_state[SDL_SCANCODE_D];
+
+		joypad_input |= down << 3 | up << 2 | left << 1 | right;
+	}
+	//If neither selected low nibble reads 0xF
+	else {
+		joypad_input |= 0xF;
+	}
+}
--- src/input.h
+++ src/input.h
@@ -1,4 +1,5 @@
 #include "common.h"
+#include <SDL.h>
 
 class Input {
 public:
@@ -6,6 +7,21 @@ public:
 
 	uint8_t read_joypad();
 	void write_joypad(uint8_t byte);
+	
+	void update_buttons();
+
 private:
 	uint8_t joypad_input;
+
+	const uint8_t* keyboard_state = SDL_GetKeyboardState(nullptr);
+
+	bool start;
+	bool select;
+	bool b;
+	bool a;
+
+	bool up;
+	bool down;
+	bool left;
+	bool right;
 };
